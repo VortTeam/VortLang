69d2a65fd3c970dce1054cc2de7a16e1eb7ad12c
PingVortex <nerfshelly123@gmail.com>
2025-04-15 16:12:52 +0200

    Finally able to change variable value

--- lang/src/variables.rs
+++ lang/src/variables.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use crate::error::VortError;
 
 #[derive(Debug, Clone)]
 pub enum VariableValue {
@@ -16,8 +17,24 @@ impl VariableStore {
         Self::default()
     }
 
-    pub fn insert(&mut self, name: String, value: VariableValue) {
+    pub fn insert(&mut self, name: String, value: VariableValue) -> Result<(), VortError> {
+        if let Some(existing) = self.variables.get(&name) {
+            match (existing, &value) {
+                (VariableValue::String(_), VariableValue::Number(_)) => {
+                    return Err(VortError::RuntimeError(
+                        format!("Can't change value of a string variable ({}) to a number", name)
+                    ));
+                }
+                (VariableValue::Number(_), VariableValue::String(_)) => {
+                    return Err(VortError::RuntimeError(
+                        format!("Can't change value of a numerical variable ({}) to a string", name)
+                    ));
+                }
+                _ => {}
+            }
+        }
         self.variables.insert(name, value);
+        Ok(())
     }
 
     pub fn get(&self, name: &str) -> Option<&VariableValue> {
